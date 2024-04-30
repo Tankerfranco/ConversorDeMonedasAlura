c53b3c647954e10c97882e17db7fd22ec56d08d8
Franco <franconegrete12@gmail.com>
2024-04-30 00:15:01 -0300

    Cambios finales

--- README.md
+++ README.md
@@ -4,5 +4,10 @@ Este es un proyecto destinado a convertir un valor dado por el usuario y en base
 -  Elegir la moneda que queremos convertir
 -  Poner el monto que queremos convertir
 -  Elegir otra moneda a convertir o salir del programa
+## Datos a tener en cuenta
+- Si se ingresan valores distintos a los pedidos, el programa finalizara su ejecucion.
+- Por favor no usar valores negativos o letras.
 
 ### Muchas gracias por usar el conversor
+
+![](https://static.vecteezy.com/system/resources/thumbnails/009/903/096/small/festival-confetti-background-realistic-birthday-cap-and-confetti-explosion-design-golden-bright-confetti-on-a-transparent-background-festival-and-birthday-celebration-elements-free-png.png)
--- src/com/conversordemonedas/principal.java
+++ src/com/conversordemonedas/principal.java
@@ -2,8 +2,10 @@ package com.conversordemonedas;
 
 import com.conversordemonedas.modelos.Convertor;
 import com.conversordemonedas.modelos.DatosConvertor;
+import com.google.gson.JsonSyntaxException;
 
 import java.sql.SQLOutput;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class principal {
@@ -15,61 +17,72 @@ public class principal {
         System.out.println("Bienvenido al conversor de divisas");
         System.out.println("***********************************");
 
-        while(bandera){
-            System.out.println("1.Peso Argentino ==> Dolar");
-            System.out.println("2.Dolar ==> Peso Argentino");
-            System.out.println("3.Real ==> Dolar");
-            System.out.println("4.Dolar ==> Real");
-            System.out.println("5.Peso Chileno ==> Dolar");
-            System.out.println("6.Dolar ==> Peso Chileno");
-            System.out.println("7.Salir");
-            System.out.println("***************************");
-            System.out.println("Elija una opcion: ");
-            switch (leer.nextInt()){
-                case 1:
-                    System.out.println("Ingrese la cantidad a convertir");
-                    valor = leer.nextInt();
-                    DatosConvertor datos = convertor.conversorDeMonedas("ARS","USD", valor);
-                    System.out.println("El valor de " + valor + " ARS corresponde a " + datos.conversion_result() + " USD");
-                    break;
-                case 2:
-                    System.out.println("Ingrese la cantidad a convertir");
-                    valor = leer.nextInt();
-                    datos = convertor.conversorDeMonedas("USD","ARS", valor);
-                    System.out.println("El valor de " + valor + " USD corresponde a " + datos.conversion_result() + " ARS");
-                    break;
-                case 3:
-                    System.out.println("Ingrese la cantidad a convertir");
-                    valor = leer.nextInt();
-                    datos = convertor.conversorDeMonedas("BRL","USD", valor);
-                    System.out.println("El valor de " + valor + " BRL corresponde a " + datos.conversion_result() + " USD");
-                    break;
-                case 4:
-                    System.out.println("Ingrese la cantidad a convertir");
-                    valor = leer.nextInt();
-                    datos = convertor.conversorDeMonedas("USD","BRL", valor);
-                    System.out.println("El valor de " + valor + " USD corresponde a " + datos.conversion_result() + " BRL");
-                    break;
-                case 5:
-                    System.out.println("Ingrese la cantidad a convertir");
-                    valor = leer.nextInt();
-                    datos = convertor.conversorDeMonedas("CLP","USD", valor);
-                    System.out.println("El valor de " + valor + " CLP corresponde a " + datos.conversion_result() + " USD");
-                    break;
-                case 6:
-                    System.out.println("Ingrese la cantidad a convertir");
-                    valor = leer.nextInt();
-                    datos = convertor.conversorDeMonedas("USD","CLP", valor);
-                    System.out.println("El valor de " + valor + " USD corresponde a " + datos.conversion_result() + " CLP");
-                    break;
-                case 7:
-                    System.out.println("Gracias por usar la aplicacion!");
-                    bandera = false;
-                    break;
-                default:
-                    System.out.println("Ingrese una opcion valida");
-                    break;
+        try{
+            while(bandera){
+                System.out.println("1.Peso Argentino ==> Dolar");
+                System.out.println("2.Dolar ==> Peso Argentino");
+                System.out.println("3.Real ==> Dolar");
+                System.out.println("4.Dolar ==> Real");
+                System.out.println("5.Peso Chileno ==> Dolar");
+                System.out.println("6.Dolar ==> Peso Chileno");
+                System.out.println("7.Salir");
+                System.out.println("***************************");
+                System.out.println("Elija una opcion: ");
+                switch (leer.nextInt()){
+                    case 1:
+                        System.out.println("Ingrese la cantidad a convertir");
+                        valor = leer.nextInt();
+                        DatosConvertor datos = convertor.conversorDeMonedas("ARS","USD", valor);
+                        System.out.println("El valor de " + valor + " ARS corresponde a " + datos.conversion_result() + " USD");
+                        break;
+                    case 2:
+                        System.out.println("Ingrese la cantidad a convertir");
+                        valor = leer.nextInt();
+                        datos = convertor.conversorDeMonedas("USD","ARS", valor);
+                        System.out.println("El valor de " + valor + " USD corresponde a " + datos.conversion_result() + " ARS");
+                        break;
+                    case 3:
+                        System.out.println("Ingrese la cantidad a convertir");
+                        valor = leer.nextInt();
+                        datos = convertor.conversorDeMonedas("BRL","USD", valor);
+                        System.out.println("El valor de " + valor + " BRL corresponde a " + datos.conversion_result() + " USD");
+                        break;
+                    case 4:
+                        System.out.println("Ingrese la cantidad a convertir");
+                        valor = leer.nextInt();
+                        datos = convertor.conversorDeMonedas("USD","BRL", valor);
+                        System.out.println("El valor de " + valor + " USD corresponde a " + datos.conversion_result() + " BRL");
+                        break;
+                    case 5:
+                        System.out.println("Ingrese la cantidad a convertir");
+                        valor = leer.nextInt();
+                        datos = convertor.conversorDeMonedas("CLP","USD", valor);
+                        System.out.println("El valor de " + valor + " CLP corresponde a " + datos.conversion_result() + " USD");
+                        break;
+                    case 6:
+                        System.out.println("Ingrese la cantidad a convertir");
+                        valor = leer.nextInt();
+                        datos = convertor.conversorDeMonedas("USD","CLP", valor);
+                        System.out.println("El valor de " + valor + " USD corresponde a " + datos.conversion_result() + " CLP");
+                        break;
+                    case 7:
+                        System.out.println("Gracias por usar la aplicacion!");
+                        bandera = false;
+                        break;
+                    default:
+                        System.out.println("Ingrese una opcion valida");
+                        break;
+                }
             }
+        }catch (InputMismatchException e){
+            System.out.println("No se aceptan letras, solo numeros");
+        }catch (JsonSyntaxException e){
+            System.out.println("No se puede ingresar numeros negativos");
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }finally {
+            System.out.println("Cerrando aplicacion");
         }
+
     }
 }
